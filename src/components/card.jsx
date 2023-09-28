a46cb4d597b3bfe114b3f1224aef37047e15006d
Juan Ceballos <juandavi789@hotmail.com>
2023-09-27 19:12:09 -0500

    Change hardcoded text with the respective infor we are passing to the card componente from the data object array

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,18 @@
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card-container'>
       <div className='img-container'>
-        <img
-          className='img'
-          src='https://source.unsplash.com/WLxQvbMyfas'
-          alt=''
-        />
+        <img className='img' src={props.imageUrl} alt={props.location} />
       </div>
       <div className='info-container'>
         <div className='location-container'>
           <p className='location'>
-            <img src='mark.png' /> JAPAN
+            <img src='mark.png' />
+            {props.location}
           </p>
           <a
             className='google-link'
-            href='https://goo.gl/maps/1DGM5WrWnATgkSNB8'
+            href={props.googleMapsUrl}
             target='_blank'
             rel='noopener noreferrer'
           >
@@ -23,17 +20,13 @@ const Card = () => {
           </a>
         </div>
         <div className='title-container'>
-          <h1 className='title'>Mount Fuji</h1>
+          <h1 className='title'>{props.title}</h1>
         </div>
         <div className='dates-container'>
-          <p className='dates'>12 Jan, 2021 - 24 Jan, 2021</p>
+          <p className='dates'>{`${props.startDate} - ${props.endDate}`}</p>
         </div>
         <div className='description-container'>
-          <p className='description'>
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <p className='description'>{props.description}</p>
         </div>
       </div>
     </div>
