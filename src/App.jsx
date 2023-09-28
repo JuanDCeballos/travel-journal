ee72c49938e4078f38f346338cb92d27f65e2ff7
Juan Ceballos <juandavi789@hotmail.com>
2023-09-27 19:11:08 -0500

    Dynamically render every single object of the data array with their respective info and styles, this is known also as data driven developmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from './components/Navbar';
 import Card from './components/card';
+import data from './data';
 import './App.css';
 
 const App = () => {
   return (
     <div className='container'>
       <Navbar />
-      <Card />
+      {data.map((el) => (
+        <Card key={el.id} {...el} />
+      ))}
     </div>
   );
 };
